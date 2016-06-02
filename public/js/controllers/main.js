a49cd633cc082f36e28eebc4cb53a8b0e3068246
mostafa <engmostafa.cs@gmail.com>
2016-06-02 14:02:55 +0200

    first commit

---
+++ public/js/controllers/main.js
@@ -0,0 +1,46 @@
+angular.module('demoController', [])
+
+	// inject the Todo service factory into our controller
+	.controller('mainController', ['$scope','$http','Todos', function($scope, $http, Todos) {
+		$scope.formData = {};
+		$scope.loading = true;
+
+
+		Todos.get()
+			.success(function(data) {
+				$scope.todos = data;
+				$scope.loading = false;
+			});
+
+		$scope.createTodo = function() {
+
+			// validate the formData to make sure that something is there
+			// if form is empty, nothing will happen
+			if ($scope.formData.text != undefined) {
+				$scope.loading = true;
+
+				// call the create function from our service (returns a promise object)
+				Todos.create($scope.formData)
+
+					// if successful creation, call our get function to get all the new todos
+					.success(function(data) {
+						$scope.loading = false;
+						$scope.formData = {}; // clear the form so our user is ready to enter another
+						$scope.todos = data; // assign our new list of todos
+					});
+			}
+		};
+
+		// DELETE ==================================================================
+		// delete a todo after checking it
+		$scope.deleteTodo = function(id) {
+			$scope.loading = true;
+
+			Todos.delete(id)
+				// if successful creation, call our get function to get all the new todos
+				.success(function(data) {
+					$scope.loading = false;
+					$scope.todos = data; // assign our new list of todos
+				});
+		};
+	}]);
